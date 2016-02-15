f8f4d774c883383c2912654f70318d7a67c0d44d
liuranchao <kevinliu@qeeka.com>
2016-02-15 13:36:07 +0800

    增加TimeUtils和DateUtils

---
+++ app/src/main/java/com/lrchao/realsejcenter/TimeUtils.java
@@ -0,0 +1,15 @@
+package com.lrchao.realsejcenter;
+
+import android.util.Log;
+
+/**
+ * Description: TODO
+ *
+ * @author liuranchao
+ */
+public class TimeUtils {
+
+    public static void log() {
+        Log.e("TimeUtils", "ttttttttttt");
+    }
+}
---
+++ mylibrary/src/main/java/com/lrchao/mylibrary/DateUtils.java
@@ -0,0 +1,15 @@
+package com.lrchao.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Description: TODO
+ *
+ * @author liuranchao
+ */
+public class DateUtils {
+
+    public static void log () {
+        Log.e("DateUtils", "ddddddddd");
+    }
+}
